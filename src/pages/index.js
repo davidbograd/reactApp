de9c00276ddc5d89554f9e18ed4ff9c83971d8a4
David Bograd <david.bjorn.bograd@gmail.com>
2018-09-14 14:16:58 +0100

    basic css styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,19 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
+
   <div>
-    <h1>Hi people test</h1>
-    <p>Great work</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porta mi a magna tristique efficitur. Donec sodales sapien non felis laoreet tempus. Mauris et dui elit. Etiam feugiat, libero non ultrices auctor, augue risus bibendum orci, eget auctor turpis diam nec nulla. Mauris ut purus rutrum, scelerisque ante et, sollicitudin orci. Sed scelerisque ut magna non dictum. Praesent condimentum accumsan dui, non eleifend est convallis eget. Fusce interdum congue ullamcorper. Vivamus eget varius mi.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="colorLine">
+    </div>
+    <div className="contentWrapper">
+      <div className="titleArea">
+        <h1>Bitesized Wisdom</h1>
+        <p>Interesting thoughts from around the web.</p>
+      </div>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porta mi a magna tristique efficitur. Donec sodales sapien non felis laoreet tempus. Mauris et dui elit. Etiam feugiat, libero non ultrices auctor, augue risus bibendum orci, eget auctor turpis diam nec nulla. Mauris ut purus rutrum, scelerisque ante et, sollicitudin orci. Sed scelerisque ut magna non dictum. Praesent condimentum accumsan dui, non eleifend est convallis eget. Fusce interdum congue ullamcorper. Vivamus eget varius mi.</p>
+      <Link to="/page-2/">Go to page 2</Link>
+    </div>
   </div>
 )
 
